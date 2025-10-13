c2b2022d1bad48e82453d6251f32fe15fa632532
Elrorri <roezgonu@gmail.com>
2025-10-13 17:47:53 -0300

    3.1 hecho

--- src/reserva.cpp
+++ src/reserva.cpp
@@ -2,16 +2,26 @@
 #include "../include/reserva.h"
 
 struct rep_reserva {
+  TSocio socio;
+  TLibro libro;
 };
 
 TReserva crearTReserva(TSocio socio, TLibro libro){
-  return NULL;
-}
+  TReserva nueva = new rep_reserva;
+  nueva->libro=libro;
+  nueva->socio=socio;
+  return nueva; }
 
 void imprimirTReserva(TReserva reserva){
+  // Reserva de libro <titulo libro> por <nombre socio> <apellido socio>.
+  printf ("Reserva de libro %s por %s %s.\n", tituloTLibro(reserva->libro),nombreTSocio(reserva->socio),apellidoTSocio(reserva->socio));
 }
 
 void liberarTReserva(TReserva &reserva){
+  liberarTLibro(reserva->libro);
+  liberarTSocio(reserva->socio);
+  delete reserva;
+  reserva = NULL;
 }
 
 TSocio socioTReserva(TReserva reserva){
