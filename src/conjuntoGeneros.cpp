608408d353ef5322b49ae7deaaabee5356c2ee57
Elrorri <roezgonu@gmail.com>
2025-10-16 00:14:37 -0300

    modulo conjuntoGeneros terminado con errores

--- src/conjuntoGeneros.cpp
+++ src/conjuntoGeneros.cpp
@@ -1,53 +1,125 @@
 #include "../include/conjuntoGeneros.h"
 
 struct rep_conjuntogeneros{
-
+  int cantMax;
+  int ids[1000]; 
+  int cardinal;
 };
 
 TConjuntoGeneros crearTConjuntoGeneros(int cantMax){
-  return NULL;    
+  TConjuntoGeneros nuevo = new rep_conjuntogeneros;
+  nuevo->cantMax = cantMax;
+  nuevo->cardinal = 0;
+  for (int i = 0; i < cantMax; i++){
+    nuevo->ids[i] = 0;
+  }
+  return nuevo;    
 }
 
 bool esVacioTConjuntoGeneros(TConjuntoGeneros c){
-  return true;
+  if (c->cardinal == 0){
+    return true;
+  }
+  else return false;
 }
 
 void insertarTConjuntoGeneros(TConjuntoGeneros &c, int id){
-
+  if (c->cantMax > id && c->ids[id] == 0){
+    c->ids[id]=1;
+    c->cardinal++;
+  }
+  else return;
 }
 
 void borrarDeTConjuntoGeneros(TConjuntoGeneros &c, int id){
-
+  if (c->cardinal != 0 && c->ids[id] == 1){
+    c->ids[id]=0;
+    c->cardinal--;
+  }
+  else return;
 }
 
 bool perteneceTConjuntoGeneros(TConjuntoGeneros c, int id){
-  return true;
+  if (c == NULL) return false;
+  if (id < 0 || id >= c->cantMax) return false;
+  return c->ids[id] == 1;
 }
 
 int cardinalTConjuntoGeneros(TConjuntoGeneros c){
-  return 0;
+  int cardinal = c->cardinal;
+  return cardinal;
 }
 
 int cantMaxTConjuntoGeneros(TConjuntoGeneros c){
-  return 0;
+  int cantMax = c->cantMax;
+  return cantMax;
 }
 
 void imprimirTConjuntoGeneros(TConjuntoGeneros c){
-
+  int cont=0;
+  for (int i=0;i<c->cantMax;i++){
+    if (c->ids[i] == 1){
+      if (cont==0){
+      printf("%d",i);
+      cont++;}
+      else printf(" %d",i);
+    }
+  }
+  printf("\n");
 }
 
 void liberarTConjuntoGeneros(TConjuntoGeneros &c){
-
+  delete c;
+  c = NULL; 
 }
 
 TConjuntoGeneros unionTConjuntoGeneros(TConjuntoGeneros c1, TConjuntoGeneros c2){
-  return NULL;
+  TConjuntoGeneros un = new rep_conjuntogeneros;
+  un->cardinal=0;
+  if (c1->cantMax>=c2->cantMax ){
+    un->cantMax=c1->cantMax;
+  }else{
+    un->cantMax=c2->cantMax;
+  }
+  for (int i=0;i<un->cantMax;i++){
+    if (c1->ids[i]==1||c2->ids[i]==1){
+      un->ids[i]=1;
+      un->cardinal++;
+    }
+  }
+  return un;
 }
 
 TConjuntoGeneros interseccionTConjuntoGeneros(TConjuntoGeneros c1, TConjuntoGeneros c2){
-  return NULL;
+  TConjuntoGeneros in = new rep_conjuntogeneros;
+  in->cardinal=0;
+  if (c1->cantMax>=c2->cantMax ){
+    in->cantMax=c1->cantMax;
+  }else{
+    in->cantMax=c2->cantMax;
+  }
+  for (int i=0;i<in->cantMax;i++){
+    if (c1->ids[i]==1&&c2->ids[i]==1){
+      in->ids[i]=1;
+      in->cardinal++;
+    }
+  }
+  return in;
 }
 
 TConjuntoGeneros diferenciaTConjuntoGeneros(TConjuntoGeneros c1, TConjuntoGeneros c2){
-  return NULL;
+  TConjuntoGeneros dif = new rep_conjuntogeneros;
+  dif->cardinal=0;
+  if (c1->cantMax>=c2->cantMax ){
+    dif->cantMax=c1->cantMax;
+  }else{
+    dif->cantMax=c2->cantMax;
+  }
+  for (int i=0;i<dif->cantMax;i++){
+    if (c1->ids[i]!=c2->ids[i]){
+      dif->ids[i]=1;
+      dif->cardinal++;
+    }
+  }
+  return dif;
 }
