34ec65f4d7d0c39238f672a6d5040b28e5ca6561
Elrorri <roezgonu@gmail.com>
2025-10-21 15:24:27 -0300

    reservas terminado

--- src/colaReservas.cpp
+++ src/colaReservas.cpp
@@ -1,32 +1,85 @@
 #include "../include/colaReservas.h"
 
+struct cola{
+  TReserva reserva;
+  cola* sig;
+};
+
 struct rep_colaReservas {
+  cola* primero;
+  cola* ultimo;
 };
 
 TColaReservas crearTColaReservas(){
-  return NULL;
+  TColaReservas nuevaCola = new rep_colaReservas;
+  nuevaCola->primero = NULL;
+  nuevaCola->ultimo = NULL;
+  return nuevaCola;
 }
 
 void encolarTColaReservas(TColaReservas &colaReservas, TReserva reserva){
+  cola* nuevaCola = new cola;
+  nuevaCola->reserva = reserva;
+  nuevaCola->sig = NULL;
+  
+  if (colaReservas->primero == NULL){
+    colaReservas->primero = nuevaCola;
+    colaReservas->ultimo = nuevaCola;
+  } else {
+    colaReservas->ultimo->sig = nuevaCola;
+    colaReservas->ultimo = nuevaCola;
+  }
 }
 
 void desencolarTColaReservas(TColaReservas &colaReservas) {
+  if (colaReservas->primero == NULL) return;
+  
+  cola* temp = colaReservas->primero;
+  colaReservas->primero = colaReservas->primero->sig;
+  
+  if (colaReservas->primero == NULL){
+    colaReservas->ultimo = NULL;
+  }
+  
+  liberarTReserva(temp->reserva);
+  delete temp;
 }
 
 TReserva frenteTColaReservas(TColaReservas colaReservas) {
-    return NULL;
+  if (colaReservas->primero == NULL) return NULL;
+  return colaReservas->primero->reserva;
 }
 
 nat cantidadTColaReservas(TColaReservas colaReservas){
-    return 0;
+  nat cont = 0;
+  cola* actual = colaReservas->primero;
+  while (actual != NULL) {
+    cont++;
+    actual = actual->sig;
+  }
+  return cont;
 }
 
 void imprimirTColaReservas(TColaReservas colaReservas){
+  printf("Cola de Reservas:\n");
+  cola* actual = colaReservas->primero;
+  while (actual != NULL) {
+    imprimirTReserva(actual->reserva);
+    actual = actual->sig;
+  }
 }
 
 void liberarTColaReservas(TColaReservas &colaReservas){
+  while (colaReservas->primero != NULL) {
+    desencolarTColaReservas(colaReservas);
+  }
+  delete colaReservas;
+  colaReservas = NULL;
 }
 
 TReserva extraerFrenteTColaReservas(TColaReservas &colaReservas) {
-  return NULL;
+  if (colaReservas->primero == NULL) return NULL;
+  TReserva extraida = colaReservas->primero->reserva;
+  desencolarTColaReservas(colaReservas);
+  return extraida;
 }
